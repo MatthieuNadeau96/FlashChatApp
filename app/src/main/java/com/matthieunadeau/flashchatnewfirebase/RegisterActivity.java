83023043902a725fb68462bb68f3c8f0b40eeefd
Matthieu <matthieunadeau96@gmail.com>
2018-03-06 14:16:09 -0500

    Handling User Login with FirebaseAuth

--- app/src/main/java/com/matthieunadeau/flashchatnewfirebase/RegisterActivity.java
+++ app/src/main/java/com/matthieunadeau/flashchatnewfirebase/RegisterActivity.java
@@ -1,5 +1,7 @@
 package com.matthieunadeau.flashchatnewfirebase;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
@@ -133,13 +135,22 @@ public class RegisterActivity extends AppCompatActivity {
                 if(!task.isSuccessful()){
                     Log.d("FlashChat", "user creation failed!");
                     showErrorDialog("Registration attempt failed");
+                } else {
+                    saveDisplayName();
+                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                    finish();
+                    startActivity(intent);
                 }
             }
         });
     }
 
     // TODO: Save the display name to Shared Preferences
-
+    private void saveDisplayName() {
+        String displayName = mUsernameView.getText().toString();
+        SharedPreferences prefs = getSharedPreferences(CHAT_PREFS, 0);
+        prefs.edit().putString(DISPLAY_NAME_KEY, displayName).apply();
+    }
 
     // TODO: Create an alert dialog to show in case registration failed
     private void showErrorDialog(String message){
