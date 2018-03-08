55cf3fe72d9ac3df467bb85834965fa5a5ace181
Matthieu <matthieunadeau96@gmail.com>
2018-03-08 13:07:00 -0500

    Retriving Data from Firebase with a ChildEventListener

--- app/src/main/java/com/matthieunadeau/flashchatnewfirebase/ChatListAdapter.java
+++ app/src/main/java/com/matthieunadeau/flashchatnewfirebase/ChatListAdapter.java
@@ -9,7 +9,9 @@ import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 
 import org.w3c.dom.Text;
@@ -27,11 +29,43 @@ public class ChatListAdapter extends BaseAdapter {
     private String mDisplayName;
     private ArrayList<DataSnapshot> mSnapshotList;
 
+    private ChildEventListener mListener = new ChildEventListener() {
+        @Override
+        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+            mSnapshotList.add(dataSnapshot);
+            notifyDataSetChanged();
+
+        }
+
+        @Override
+        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+        }
+
+        @Override
+        public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+        }
+
+        @Override
+        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+
+        }
+    };
+
     public ChatListAdapter(Activity activity, DatabaseReference ref, String name) {
 
         mActivity = activity;
         mDisplayName = name;
         mDatabaseReference = ref.child("messages");
+        mDatabaseReference.addChildEventListener(mListener);
+
         mSnapshotList = new ArrayList<>();
 
     }
@@ -44,12 +78,14 @@ public class ChatListAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return 0;
+        return mSnapshotList.size();
     }
 
     @Override
     public InstantMessage getItem(int position) {
-        return null;
+
+        DataSnapshot snapshot = mSnapshotList.get(position);
+        return snapshot.getValue(InstantMessage.class);
     }
 
     @Override
@@ -83,5 +119,8 @@ public class ChatListAdapter extends BaseAdapter {
         return convertView;
     }
 
+    public void cleanup() {
+        mDatabaseReference.removeEventListener(mListener);
+    }
 
 }
